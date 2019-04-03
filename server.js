8edf496bde53d22338668d9aa6942774725daafc
kth <katiehaster@gmail.com>
2019-04-03 12:37:05 -0700

    .gitignore add

--- server.js
+++ server.js
@@ -3,8 +3,21 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 // Data
+// ========================================================
+
+
+
+
+
+// Routes
+// ========================================================
+
+
+
+
+// Listener
 // ===========================================================
-
-
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
